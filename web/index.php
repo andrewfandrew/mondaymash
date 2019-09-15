fa9a2fae26546353b3d680ccde3fbdc5aef89ad5
Andrew Farquharson <andrew.farquharson@gmail.com>
2019-09-15 16:43:36 +0100

    create FrontPage class

--- web/index.php
+++ web/index.php
@@ -11,12 +11,12 @@ function my_autoloader($class) {
 spl_autoload_register('my_autoloader');
 include('Test.php');
 
-$obj = new Test();
-$obj->output();
+$obj = new FrontPage();
+$heading1 = $obj->printHeading1('Heading One $$££');
 
 ?>
 
-<div class="page-header"><h1>Content Header</h1></div>
+<div class="page-header"><h1><?= $heading1 ?></h1></div>
 <p>This is where the page-specific content goes. This section, and the corresponding header, will change from one page to the next.</p>
 
 <p>Volutpat at varius sed sollicitudin et, arcu. Vivamus viverra. Nullam turpis. Vestibulum sed etiam. Lorem ipsum sit amet dolore. Nulla facilisi. Sed tortor. Aenean felis. Quisque eros. Cras lobortis commodo metus. Vestibulum vel purus. In eget odio in sapien adipiscing blandit. Quisque augue tortor, facilisis sit amet, aliquam, suscipit vitae, cursus sed, arcu lorem ipsum dolor sit amet.</p>
